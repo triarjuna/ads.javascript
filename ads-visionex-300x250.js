45a943e670ead80165ff039044f160386915cc58
ekayudistira <eka.yudistira.pandawa@gmail.com>
2021-06-15 08:11:55 +0700

    commit

---
+++ ads-visionex-300x250.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '724b8dd444db8acbeeeca2d6330a4618',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://hottercensorbeaker.com/724b8dd444db8acbeeeca2d6330a4618/invoke.js"></scr' + 'ipt>');
+</script>
