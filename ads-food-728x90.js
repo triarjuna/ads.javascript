4262cd6a0991f3809d2eaf788d5ef2eda5ffaf6b
ekayudistira <eka.yudistira.pandawa@gmail.com>
2021-06-15 11:43:34 +0700

    upadate

---
+++ ads-food-728x90.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '15977cfd7d2bb67f7441c80aa6a871d5',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://hottercensorbeaker.com/15977cfd7d2bb67f7441c80aa6a871d5/invoke.js"></scr' + 'ipt>');
