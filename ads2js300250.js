4a8b405ccc9eef34b98d0984af12d3dce804ab04
ekayudistira <eka.yudistira.pandawa@gmail.com>
2021-06-15 11:34:42 +0700

    create ads2

---
+++ ads2js300250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '271defe678707597ff8c579e137edc63',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://lavatoryhitschoolmaster.com/271defe678707597ff8c579e137edc63/invoke.js"></scr' + 'ipt>');
